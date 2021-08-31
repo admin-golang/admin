f38e5cd8dd5ec70e5bf5765ea1254ef19cf05913
Chris Ramón <rchristian.ramon@gmail.com>
2021-08-31 00:55:00 -0500

    templates/materialUI: initial list page iteration

--- templates/materialUI.tsx
+++ templates/materialUI.tsx
@@ -31,6 +31,12 @@ const {
 	Alert,
 	Menu,
 	MenuItem,
+  TableContainer,
+  Table,
+  TableBody,
+  TableRow,
+  TableCell,
+	TableFooter
 } = MaterialUI;
 
 const {
@@ -453,6 +459,7 @@ function [[ .page.ID ]]Dashboard() {
 }
 [[ end ]]
 
+
 [[ define "List" ]]
 function [[ .page.ID ]]List() {
   const [open, setOpen] = React.useState(true);
@@ -460,6 +467,46 @@ function [[ .page.ID ]]List() {
     setOpen(!open);
   };
 
+	function createData(name: string, calories: number, fat: number) {
+	  return { name, calories, fat };
+	}
+	
+	const rows = [
+	  createData('Cupcake', 305, 3.7),
+	  createData('Donut', 452, 25.0),
+	  createData('Eclair', 262, 16.0),
+	  createData('Frozen yoghurt', 159, 6.0),
+	  createData('Gingerbread', 356, 16.0),
+	  createData('Honeycomb', 408, 3.2),
+	  createData('Ice cream sandwich', 237, 9.0),
+	  createData('Jelly Bean', 375, 0.0),
+	  createData('KitKat', 518, 26.0),
+	  createData('Lollipop', 392, 0.2),
+	  createData('Marshmallow', 318, 0),
+	  createData('Nougat', 360, 19.0),
+	  createData('Oreo', 437, 18.0),
+	].sort((a, b) => (a.calories < b.calories ? -1 : 1));
+
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+  // Avoid a layout jump when reaching the last page with empty rows.
+  const emptyRows =
+    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
+
+  const handleChangePage = (
+    event: React.MouseEvent<HTMLButtonElement> | null,
+    newPage: number,
+  ) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+  ) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
 
   return (
     <Box sx={{ display: 'flex' }}>
@@ -546,30 +593,46 @@ function [[ .page.ID ]]List() {
         <Toolbar />
         <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
           <Grid container spacing={3}>
-            <Grid item xs={12} md={8} lg={9}>
+            <Grid item xs={12} md={12} lg={12}>
               <Paper
                 sx={{
                   p: 2,
                   display: 'flex',
                   flexDirection: 'column',
-                  height: 240,
+                  height: 640,
                 }}
               >
-              </Paper>
-            </Grid>
-            <Grid item xs={12} md={4} lg={3}>
-              <Paper
-                sx={{
-                  p: 2,
-                  display: 'flex',
-                  flexDirection: 'column',
-                  height: 240,
-                }}
-              >
-              </Paper>
-            </Grid>
-            <Grid item xs={12}>
-              <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column' }}>
+    						<TableContainer component={Paper}>
+    						  <Table sx={{ minWidth: 500 }} aria-label="custom pagination table">
+    						    <TableBody>
+    						      {(rowsPerPage > 0
+    						        ? rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+    						        : rows
+    						      ).map((row) => (
+    						        <TableRow key={row.name}>
+    						          <TableCell component="th" scope="row">
+    						            {row.name}
+    						          </TableCell>
+    						          <TableCell style={{ width: 160 }} align="right">
+    						            {row.calories}
+    						          </TableCell>
+    						          <TableCell style={{ width: 160 }} align="right">
+    						            {row.fat}
+    						          </TableCell>
+    						        </TableRow>
+    						      ))}
+    						      {emptyRows > 0 && (
+    						        <TableRow style={{ height: 53 * emptyRows }}>
+    						          <TableCell colSpan={6} />
+    						        </TableRow>
+    						      )}
+    						    </TableBody>
+    						    <TableFooter>
+    						      <TableRow>
+    						      </TableRow>
+    						    </TableFooter>
+    						  </Table>
+    						</TableContainer>
               </Paper>
             </Grid>
           </Grid>
